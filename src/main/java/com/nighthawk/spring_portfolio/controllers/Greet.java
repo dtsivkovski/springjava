743eba52122f28112d1aea403a76d2fe565e20d2
dtsivkovski <dtsivkovs@gmail.com>
2022-09-16 09:03:38 -0700

    modified the greet page

--- src/main/java/com/nighthawk/spring_portfolio/controllers/Greet.java
+++ src/main/java/com/nighthawk/spring_portfolio/controllers/Greet.java
@@ -14,10 +14,19 @@ public class Greet {
     // @GetMapping handles GET request for /greet, maps it to greeting() method
     @GetMapping("/greet")
     // @RequestParam handles variables binding to frontend, defaults, etc
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+    public String greeting(@RequestParam(name="num1", required=false, defaultValue="0") String num1, @RequestParam(name="num2", required=false, defaultValue="0") String num2, Model model) {
 
         // model attributes are visible to Thymeleaf when HTML is "pre-processed"
-        model.addAttribute("name", name);
+        model.addAttribute("num1", num1);
+        model.addAttribute("num2", num2);
+        // Convert nums to int
+        int int1 = Integer.parseInt(num1);
+        int int2 = Integer.parseInt(num2);
+        int sum =  int1 + int2;
+        // add sum attribute
+        // model.addAttribute("int1", int1);
+        // model.addAttribute("int2", int2);
+        model.addAttribute("sum", sum);
 
         // load HTML VIEW (greet.html)
         return "greet"; 
