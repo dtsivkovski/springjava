7f1a72c58b22774f79b0920bebdbc5b59aa953e6
dtsivkovski <dtsivkovs@gmail.com>
2022-11-25 19:49:53 -0800

    changes so far

---
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/person/Person.java
@@ -0,0 +1,108 @@
+package com.nighthawk.spring_portfolio.mvc.person;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.GregorianCalendar;
+import java.text.SimpleDateFormat;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.vladmihalcea.hibernate.type.json.JsonType;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+/*
+Person is a POJO, Plain Old Java Object.
+First set of annotations add functionality to POJO
+--- @Setter @Getter @ToString @NoArgsConstructor @RequiredArgsConstructor
+The last annotation connect to database
+--- @Entity
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@TypeDef(name="json", typeClass = JsonType.class)
+public class Person {
+    
+    // automatic unique identifier for Person record
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    // email, password, roles are key attributes to login and authentication
+    @NotEmpty
+    @Size(min=5)
+    @Column(unique=true)
+    @Email
+    private String email;
+
+    @NotEmpty
+    private String password;
+
+    // @NonNull, etc placed in params of constructor: "@NonNull @Size(min = 2, max = 30, message = "Name (2 to 30 chars)") String name"
+    @NonNull
+    @Size(min = 2, max = 30, message = "Name (2 to 30 chars)")
+    private String name;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date dob;
+    
+
+    /* HashMap is used to store JSON for daily "stats"
+    "stats": {
+        "2022-11-13": {
+            "calories": 2200,
+            "steps": 8000
+        }
+    }
+    */
+    @Type(type="json")
+    @Column(columnDefinition = "jsonb")
+    private Map<String,Map<String, Object>> stats = new HashMap<>(); 
+    
+
+    // Constructor used when building object from an API
+    public Person(String email, String password, String name, Date dob) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.dob = dob;
+    }
+
+    // A custom getter to return age from dob attribute
+    public int getAge() {
+        if (this.dob != null) {
+            LocalDate birthDay = this.dob.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+            return Period.between(birthDay, LocalDate.now()).getYears(); }
+        return -1;
+    }
+
+    // main class as a tester
+    public static void main(String[] args) {
+        Person p1 = new Person();
+        Date dob2 = new GregorianCalendar(2005, 3, 4).getTime();
+        Person p2 = new Person("johnny@gmail.com", "123johnny", "Johnny Coder", dob2);
+        System.out.println(p1);
+        System.out.println(p2);
+     }
+
+}
