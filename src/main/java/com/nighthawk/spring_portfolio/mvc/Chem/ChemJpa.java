9271bc508fba0bae07a2a3695d9d6fc73886771c
evmyclipz <mrohan2005@gmail.com>
2023-02-15 09:53:09 -0800

    Rohan Chem

--- src/main/java/com/nighthawk/spring_portfolio/mvc/Chem/ChemJpa.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/Chem/ChemJpa.java
@@ -6,10 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 // JPA is an object-relational mapping (ORM) to persistent data, originally relational databases (SQL). Today JPA implementations has been extended for NoSQL.
 public interface ChemJpa extends JpaRepository<ChemObject, Integer> {
-    // JPA has many built in methods, these few have been prototyped for this application
+    // JPA has many built in methods, these few have been prototyped for this
+    // application
     // void save(PhysObject pobj);
     // List<PhysObject> findAllByOrderByuserIDAsc();
-    List<ChemObject> findByowner(String username);
+    List<ChemObject> findByOwner(String owner);
 
     // List<ChemObject> findById(int id);
 }
--- src/main/java/com/nighthawk/spring_portfolio/mvc/Chem/ChemObject.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/Chem/ChemObject.java
@@ -5,62 +5,73 @@ import java.util.Map;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import javax.validation.constraints.NotEmpty;
 
 import javax.persistence.*;
 
-@Data  // Annotations to simplify writing code (ie constructors, setters)
+@Data // Annotations to simplify writing code (ie constructors, setters)
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 public class ChemObject {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
-    private ChemJpa repository;
+    private Integer id;
+    // private ChemJpa repository;
+    @NotEmpty
     private String owner;
+
+    @NotEmpty
     private double mass;
-    private double moles;
-    private double density;
+
+    @NotEmpty
     private double volume;
+
+    @NotEmpty
     private double molecularWeight;
-    private double recentDensity;
-    private double recentMole;
+
+    private double density;
+    private double mole;
     // Hashmap with type and result for history of calculations on the object
     @ElementCollection(fetch = FetchType.EAGER)
     @MapKeyColumn(name = "type")
     @Column(name = "result")
     private Map<String, Double> history = new HashMap<String, Double>();
 
-
     // Initializes object with mass and username
-    ChemObject(double m, String username) {
-        mass = m;
-        owner = username;
+    ChemObject(double m, double v, double moleW, String username) {
+        this.mass = m;
+        this.volume = v;
+        this.molecularWeight = moleW;
+        this.owner = username;
+        this.density = calculateDensity(volume);
+        this.mole = calculateMole(moleW);
+
     }
 
-    // Puts calculations in
-    public void addCalculation(String typeinput, double result) {
-        history.put(typeinput, result);
-    }
+    /*
+     * // Puts calculations in
+     * public void addCalculation(String typeinput, double result) {
+     * history.put(typeinput, result);
+     * }
+     */
 
     public double calculateDensity(double volume) {
-        double density = mass/volume;
+        double density = mass / volume;
         System.out.println("Density Calculated");
-        recentDensity = density;
-        String typeInp = "D (mass = " + mass + ", volume = " + volume + ")";
-        addCalculation(typeInp, density);
+        // recentDensity = density;
+        // String typeInp = "D (mass = " + mass + ", volume = " + volume + ")";
+        // addCalculation(typeInp, density);
         return density;
     }
-      
+
     public double calculateMole(double molecularWeight) {
-        double mole = mass/molecularWeight;
+        double mole = mass / molecularWeight;
         System.out.println("Mole Calculated");
-        recentMole = mole;
-        String typeInp = "M (mass = " + mass + ", molecular weight = " + molecularWeight + ")";
-        addCalculation(typeInp, mole);
+        // recentMole = mole;
+        // String typeInp = "M (mass = " + mass + ", molecular weight = " +
+        // molecularWeight + ")";
+        // addCalculation(typeInp, mole);
         return mole;
     }
 }
-    
-
-
