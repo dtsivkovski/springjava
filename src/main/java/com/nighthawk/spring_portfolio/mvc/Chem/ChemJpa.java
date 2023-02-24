f5758117fab3c99486152e2a8acb578a1f4c9f3f
evmyclipz <mrohan2005@gmail.com>
2023-02-23 17:08:21 -0800

    smt

--- src/main/java/com/nighthawk/spring_portfolio/mvc/Chem/ChemJpa.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/Chem/ChemJpa.java
@@ -2,12 +2,7 @@ package com.nighthawk.spring_portfolio.mvc.Chem;
 
 import java.util.List;
 
-import javax.transaction.Transactional;
-
-import org.springframework.data.jpa.repository.Modifying;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 // JPA is an object-relational mapping (ORM) to persistent data, originally relational databases (SQL). Today JPA implementations has been extended for NoSQL.
 public interface ChemJpa extends JpaRepository<ChemObject, Integer> {
